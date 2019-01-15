5522868c0cee0301f72f2020d7479c6f6cd6a8e2
LeTanThanh <tanthanhcr95@gmail.com>
2019-01-15 16:54:31 +0700

    use_wicked_pdf_for_orders_show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,17 @@
 class OrdersController < ApplicationController
   def show
     @order = Order.find_by id: params[:id]
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        disposition = (params[:download] == "true") ? "attachment" : "inline"
+
+        render pdf: "order #{@order.token}",
+          disposition: disposition,
+          template: "orders/show",
+          layout: "layouts/application"
+      end
+    end
   end
 end
